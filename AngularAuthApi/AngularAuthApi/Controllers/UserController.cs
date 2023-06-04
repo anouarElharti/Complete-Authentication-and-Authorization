fd70c7e8ca94e62ac2031e0dda3f09546d24a414
Elharti <AElharti@lims.com>
2023-06-04 18:44:45 +0000

    Sql server migrations, creating User model, User Apis in the User Controller

---
+++ AngularAuthApi/AngularAuthApi/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using AngularAuthApi.Context;
+using AngularAuthApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+
+        public UserController(AppDbContext authContext)
+        {
+            _authContext = authContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<ActionResult> Authenticate([FromBody] User userObj)
+        {
+            if(userObj == null)
+                return BadRequest();
+
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.Username == userObj.Username && x.Password == userObj.Password);
+            
+            if (user == null)
+                return NotFound(new { Message = "User Not Found!" });
+
+            return Ok(new { Message = "Login Success!" });
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if(userObj==null)
+                return BadRequest();
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(new { Message = "User registered" });
+        }
+    }
+}
